bbfce3986c0a5863014b9a6daa1ba03225139d43
Erik Peng <wheelo@163.com>
2018-04-24 14:46:38 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Application } from './components/Application.js'
+
+const root = document.querySelector('main')
+
+
+ReactDOM.render(<Application />, root)
